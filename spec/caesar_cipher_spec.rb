b4fe1415af45a4b9e17c483cb33f667131c05fe1
Michael Dijamco <mdd233@cornell.edu>
2015-09-04 14:33:28 -0400

    Caesar cipher v1 passes all tests; needs refactoring now

--- spec/caesar_cipher_spec.rb
+++ spec/caesar_cipher_spec.rb
@@ -5,8 +5,16 @@ describe "caesar cipher" do
 		caesar_cipher('a',3).should == 'd'
 	end
 
+	it "accounts for nonletters" do
+		caesar_cipher('a!123_+',3).should == 'd!123_+'
+	end
+
 	it "shift each character in a word right, no z to a wrap" do
 		caesar_cipher('cat',5).should == 'hfy'
+	end
+
+	it "wrap z to a" do
+		caesar_cipher("z",1).should == 'a'
 	end
 
 	it "wrap z to a" do
@@ -18,10 +26,10 @@ describe "caesar cipher" do
 	end
 
 	it "keeps the same case and wraps z to a" do
-		caesar_cipher("CoMpUtEr",7).should == 'CvMwUaLy'
+		caesar_cipher("CoMpUtEr",7).should == 'JvTwBaLy'
 	end
 
-	it "accounts for nonletters" do
-		caesar_cipher("What a string!",5) == 'Bmfy f xywnsl!'
+	it "does everything at once" do
+		caesar_cipher("What a string!123",5) == 'Bmfy f xywnsl!123'
 	end
 end
