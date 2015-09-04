66622ca683793425dc64d12241282b9ed5318074
Michael Dijamco <mdd233@cornell.edu>
2015-09-04 13:41:08 -0400

    Manually added rspec functionality

---
+++ lib/caesar_cipher.rb
@@ -0,0 +1,5 @@
+class MyModel
+  def the_truth
+    1
+  end
+end
---
+++ spec/lib/caesar_cipher_spec.rb
@@ -0,0 +1,8 @@
+require "spec_helper"
+require "caesar_cipher"
+
+describe MyModel do
+  it "should be true" do
+    MyModel.new.the_truth.should == 1
+  end
+end
